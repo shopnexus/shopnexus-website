142b17f0f188eb5beccfa729a86f4f8740745531
Đậu Khoa <khoakomlem@gmail.com>
2025-04-01 21:30:57 +0700

    handle unauthorized token

--- src/core/query-client.ts
+++ src/core/query-client.ts
@@ -1,18 +1,15 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { MutationCache, QueryCache, QueryClient } from "@tanstack/react-query"
 import { createConnectTransport } from "@connectrpc/connect-web"
 
 // import { env } from './env';
 // TODO: sua lai cho nay
 
+const CONNECT_UNAUTHORIZED_ERROR_CODE = 16
+
 function handleUnAuthorized(error: any) {
-	console.error(error)
-	// If (window.location.pathname.split('/')[1] === 'share') return;
-	// If (isServerError(error, 'ErrWrongAuthHeader')) {
-	//   location.href = '/login';
-	//   console.log('redirect to login');
-	//   // !!! có thể có loop ở đây vì nếu chuyển hướng vô /login mà cookie ko đc set thì sẽ lại chuyển hướng vô /login lần nữa
-	// }
+  if (error.code === CONNECT_UNAUTHORIZED_ERROR_CODE) {
+    location.href = '/login'
+  }
 }
 
 export const finalTransport = createConnectTransport({
