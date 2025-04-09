62c6dc842869fee86fd7d345cc0c7fd00bca8118
Đậu Khoa <khoakomlem@gmail.com>
2025-04-09 23:48:28 +0700

    add logo

--- src/app/Home/Home.tsx
+++ src/app/Home/Home.tsx
@@ -1,76 +1,81 @@
-import { Link } from "react-router-dom"
-import Button from "../../components/ui/Button"
-import FeaturedProducts from "../Products/FeaturedProducts"
-import Newsletter from "../../components/Newsletter"
-import SplitText from "../../blocks/TextAnimations/SplitText/SplitText"
-import StarBorder from "../../blocks/Animations/StarBorder/StarBorder"
-import NewProducts from "../Products/NewProducts"
-import InforFooter from "../../components/InfoFooter"
-import { useMutation, useQuery } from "@connectrpc/connect-query"
-import { listBrands, listTags, upload } from "shopnexus-protobuf-gen-ts"
-import { useRef } from "react"
+import { Link } from "react-router-dom";
+import Button from "../../components/ui/Button";
+import FeaturedProducts from "../Products/FeaturedProducts";
+import Newsletter from "../../components/Newsletter";
+import SplitText from "../../blocks/TextAnimations/SplitText/SplitText";
+import StarBorder from "../../blocks/Animations/StarBorder/StarBorder";
+import NewProducts from "../Products/NewProducts";
+import InforFooter from "../../components/InfoFooter";
+import { useMutation, useQuery } from "@connectrpc/connect-query";
+import { listBrands, listTags, upload } from "shopnexus-protobuf-gen-ts";
+import { useRef } from "react";
 
 const handleAnimationComplete = () => {
-	console.log("All letters have animated!")
-}
+  console.log("All letters have animated!");
+};
 
 export default function Home() {
-	const fileRef = useRef<HTMLInputElement>(null)
-	const { mutateAsync } = useMutation(upload)
-	const uploadFile = async (e: React.MouseEvent<HTMLButtonElement>) => {
-		e.preventDefault()
-		// debugger
-		const file = fileRef.current?.files?.[0]
-		if (!file) return
-		await mutateAsync({
-			name: file.name,
-			content: new Uint8Array(await file.arrayBuffer()),
-		})
-	}
+  const fileRef = useRef<HTMLInputElement>(null);
+  const { mutateAsync } = useMutation(upload);
+  const uploadFile = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    // debugger
+    const file = fileRef.current?.files?.[0];
+    if (!file) return;
+    await mutateAsync({
+      name: file.name,
+      content: new Uint8Array(await file.arrayBuffer()),
+    });
+  };
 
-	const { data: listTag } = useQuery(listTags, {
-		pagination: {
-			limit: 10,
-			page: 1,
-		},
-	})
+  const { data: listTag } = useQuery(listTags, {
+    pagination: {
+      limit: 10,
+      page: 1,
+    },
+  });
 
 	return (
 		<div className="flex min-h-screen flex-col items-center justify-between">
 			{/* <input ref={fileRef} type="file" placeholder="FILE TEST" />
 			<button onClick={(e) => uploadFile(e)}>TEST</button> */}
 
-			{/* Hero Section */}
-			<section className="w-full max-w-7xl mx-auto text-center">
-				<SplitText
-					text="Welcome to ShopNexus!!"
-					className="text-2xl font-semibold text-center sm:text-6xl"
-					delay={70}
-					animationFrom={{ opacity: 0, transform: "translate3d(0,50px,0)" }}
-					animationTo={{ opacity: 1, transform: "translate3d(0,0,0)" }}
-					threshold={0.2}
-					rootMargin="-50px"
-					onLetterAnimationComplete={handleAnimationComplete}
-				/>
-				<p className="mt-6 text-lg leading-8 text-gray-600">
-					Discover the perfect pair for every occasion.
-				</p>
-				<div className="mt-10 flex items-center justify-center gap-x-6 ">
-					<StarBorder
-						as="button"
-						className="custom-class "
-						color="cyan"
-						speed="5s"
-					>
-						<Link to="/products">Shop Now</Link>
-					</StarBorder>
-					<Button variant="outline">
-						<Link to="/about">
-							Learn more <span aria-hidden="true">→</span>
-						</Link>
-					</Button>
-				</div>
-			</section>
+      {/* Hero Section */}
+      <section className="w-full max-w-7xl mx-auto text-center">
+        <SplitText
+          text="Welcome to ShopNexus!!"
+          className="text-2xl font-semibold text-center sm:text-6xl"
+          delay={70}
+          animationFrom={{ opacity: 0, transform: "translate3d(0,50px,0)" }}
+          animationTo={{ opacity: 1, transform: "translate3d(0,0,0)" }}
+          threshold={0.2}
+          rootMargin="-50px"
+          onLetterAnimationComplete={handleAnimationComplete}
+        />
+        <img
+          src="/favicon.png"
+          alt="ShopNexus Logo"
+          className="w-24 mx-auto mt-4"
+        />
+        <p className="mt-6 text-lg leading-8 text-gray-600">
+          Discover the perfect pair for every occasion.
+        </p>
+        <div className="mt-10 flex items-center justify-center gap-x-6 ">
+          <StarBorder
+            as="button"
+            className="custom-class "
+            color="cyan"
+            speed="5s"
+          >
+            <Link to="/products">Shop Now</Link>
+          </StarBorder>
+          <Button variant="outline">
+            <Link to="/about">
+              Learn more <span aria-hidden="true">→</span>
+            </Link>
+          </Button>
+        </div>
+      </section>
 
 			{/* Featured Products */}
 			<section className="w-full bg-white py-16 px-4">
@@ -115,7 +120,7 @@ export default function Home() {
 			{/* Newsletter */}
 			<Newsletter />
 
-			<InforFooter />
-		</div>
-	)
+      <InforFooter />
+    </div>
+  );
 }
