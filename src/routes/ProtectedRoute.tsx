1b8c19e55636c7d2ab46899035e9af1f0d691539
Vucongchien <vucongchien204@gmail.com>
2025-04-21 22:29:17 +0700

    .... idk

--- src/routes/ProtectedRoute.tsx
+++ src/routes/ProtectedRoute.tsx
@@ -2,15 +2,21 @@ import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
 
 const ProtectedRoute = () => {
-  const { user, loading } = useAuth();
+  const { user,admin, loading } = useAuth();
 
   if (loading) {
     return null; // Hoặc có thể trả về một loading spinner
   }
 
+  if(admin){
+    return <p>you is a admin</p>
+  }
+
   if (!user) {
     return <Navigate to="/login" replace />;
   }
+
+
 
   return <Outlet />;
 };
